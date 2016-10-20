b1d3d75ab2df8483c9f4a66f82993e9afe31776b
Chris Cornutt <enygma@phpdeveloper.org>
2016-10-20 07:23:18 -0500

    Updating composer config with more info and adding illuminate/database, updating .env with DB params and adding the db.php config to load illuminate capsule

---
+++ bootstrap/db.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This does the setup for the Eloquent handling
+ * outside of Laravel (using capsule)
+ */
+
+$dbconfig = [
+    'driver'    => 'mysql',
+    'host'      => $_ENV['DB_HOST'],
+    'database'  => $_ENV['DB_NAME'],
+    'username'  => $_ENV['DB_USER'],
+    'password'  => $_ENV['DB_PASS'],
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+];
+
+$capsule = new Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($dbconfig);
+$capsule->bootEloquent();
+$capsule->setAsGlobal();
