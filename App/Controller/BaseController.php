1e05f05500d04b2bf26ae66e2ab7e6a8adbd54cb
Chris Cornutt <enygma@phpdeveloper.org>
2017-10-10 09:02:46 -0500

    Revert "adding current application"
    This reverts commit 8c30c4407090749a293d1c3ac1e34f1b4f57490c.

--- App/Controller/BaseController.php
+++ App/Controller/BaseController.php
@@ -8,7 +8,6 @@ class Basecontroller
     public function __construct($container)
     {
         $this->container = $container;
-        $this->validator = \Psecio\Validation\Validator::getInstance('request.slim3');
     }
 
     public function render($view, array $data = [])
@@ -19,42 +18,15 @@ class Basecontroller
 
     public function __get($property)
     {
-        if ($property == 'user') {
-            return $this->container->session->get('user');
-        }
         if (isset($this->container, $property)) {
             return $this->container->$property;
         }
         return null;
     }
 
-    public function verify($request, array $rules, array $messages = [])
+    public function verify($request, $rules)
     {
-        return $this->validator->execute($request, $rules, $messages);
-    }
-
-    public function jsonResponse($status, $message = null, array $addl = [])
-    {
-        $output = ['success' => $status];
-        if ($message !== null) {
-            $output['message'] = $message;
-        }
-        if (!empty($addl)) {
-            $output = array_merge($output, $addl);
-        }
-
-        $response = $this->response->withHeader('Content-type', 'application/json');
-        $body = $response->getBody();
-        $body->write(json_encode($output));
-
-        return $response;
-    }
-    public function jsonFail($message = null, array $addl = [])
-    {
-        return $this->jsonResponse(false, $message, $addl);
-    }
-    public function jsonSuccess($message = null, array $addl = [])
-    {
-        return $this->jsonResponse(true, $message, $addl);
+        $validator = \Psecio\Validation\Validator::getInstance('request.slim3');
+        return $validator->execute($request, $rules);
     }
 }
--- App/Controller/IndexController.php
+++ App/Controller/IndexController.php
@@ -4,30 +4,9 @@ namespace App\Controller;
 
 class IndexController extends \App\Controller\BaseController
 {
-    use \App\Traits\FlashMessage;
-
-    protected function getTwitchProvider()
-    {
-        $provider = new \Depotwarehouse\OAuth2\Client\Twitch\Provider\Twitch([
-            'clientId' => $_ENV['TWITCH_CLIENT_ID'],
-            'clientSecret' => $_ENV['TWITCH_CLIENT_SECRET'],
-            'redirectUri' => "http://9037c3d9.ngrok.io/social/twitchCallback"
-        ]);
-        return $provider;
-    }
-
     public function index()
     {
-        $provider = $this->getTwitchProvider();
-        $data = [
-            'url' => $provider->getAuthorizationUrl()
-        ];
-        return $this->render('/index/index.php', $data);
-    }
-
-    public function error()
-    {
         $data = [];
-        return $this->render('/index/error.php', $data);
+        return $this->render('/index/index.php', $data);
     }
 }
